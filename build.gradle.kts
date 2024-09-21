cdcf10377176818a91adc83519d13419d590ad3b
GooDDoGGoD <rup14pur@mail.ru>
2024-09-21 12:49:50 +0300

    Иницилизация 3й лабы

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ dependencies {
     implementation(project(":Data_Access"))
     implementation(project(":Service"))
 
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    runtimeOnly("org.postgresql:postgresql")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:3.9.0")
