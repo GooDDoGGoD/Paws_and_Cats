6643afa2746666dc4d8ea6e7baf286b03ca823b6
GooDDoGGoD <rup14pur@mail.ru>
2024-09-21 13:13:31 +0300

    Data Transfer Object

---
+++ Data_Transfer/src/main/java/ru/osipov/DTO/CatDto.java
@@ -0,0 +1,11 @@
+package ru.osipov.DTO;
+
+import java.time.LocalDate;
+
+public class CatDto {
+    private Integer id;
+    private String name;
+    private LocalDate birthDate;
+    private String breed;
+    private String color;
+}
---
+++ Data_Transfer/src/main/java/ru/osipov/DTO/OwnerDto.java
@@ -0,0 +1,9 @@
+package ru.osipov.DTO;
+
+import java.time.LocalDate;
+
+public class OwnerDto {
+    private Integer id;
+    private String name;
+    private LocalDate birthDate;
+}
